09b113d6221bf9c4c5af8499f44b977f2221d2d2
sahib memmedov <sahibmemmedov7890@gmail.com>
2024-03-18 14:29:35 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Min());
+        }
+        public static int Min()
+        {
+            int[] number = { 1, 2, 35, 47, 89, 64, 45 };
+            int min = number[0];           
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] < min)
+                {
+                    min = number[i];                  
+                }
+            }
+            return min;
+        }
+    }
+}
